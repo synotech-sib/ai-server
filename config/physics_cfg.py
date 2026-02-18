281965cf8b2a3ae2c6367a3d9f347640f4388a8b
synotech-sib <wschoi@synotech.co.kr>
2026-02-18 12:32:07 +0900

    Add files via upload

--- config/physics_cfg.py
+++ config/physics_cfg.py
@@ -1,9 +1,7 @@
 # config/physics_cfg.py
-
-# SIB 물리 상수 및 한계치
 MIN_LOADING = 1.0
 MAX_LOADING = 50.0
 DEFAULT_NP_RATIO = 1.1
 
-# 유효성 검사 기준
-VALIDATION_THRESHOLD = 0.05  # 5% 오차 허용
+# [Step 7 추가] SIB 표준 전압 (평균 3.2V 가정)
+NOMINAL_VOLTAGE = 3.2
