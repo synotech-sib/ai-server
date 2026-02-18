54bab6be23292936f5e765828dea512cce457008
synotech-sib <wschoi@synotech.co.kr>
2026-02-18 11:53:53 +0900

    Update security_cfg.py

--- config/security_cfg.py
+++ config/security_cfg.py
@@ -1,1 +1,10 @@
+# config/security_cfg.py
 
+ADMIN_PASSWORD = "synotech0773!"
+WHITELIST_IDS = ["wschoi", "admin_syno"]
+
+# 보안 모드 상태
+SECURITY_MODE = "Active"
+
+def check_admin(pw):
+    return pw == ADMIN_PASSWORD
