3ec8e8c7b61ebc96563176d7ad6b0440b2421332
synotech-sib <wschoi@synotech.co.kr>
2026-02-18 11:55:37 +0900

    Update engine.py

--- modules/engine.py
+++ modules/engine.py
@@ -1,1 +1,13 @@
+# modules/engine.py
+from config.physics_cfg import DEFAULT_NP_RATIO
 
+def calculate_battery_specs(loading, capacity, area, np_ratio=DEFAULT_NP_RATIO):
+    areal_capacity = (loading * capacity) / 1000
+    total_capacity = areal_capacity * area
+    required_anode = areal_capacity * np_ratio
+    
+    return {
+        "areal_capacity": round(areal_capacity, 3),
+        "total_capacity": round(total_capacity, 2),
+        "required_anode": round(required_anode, 3)
+    }
