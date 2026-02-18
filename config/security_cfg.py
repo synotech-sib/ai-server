24117a3bb1e313fafc14b905c60b453e08b31e69
synotech-sib <wschoi@synotech.co.kr>
2026-02-18 12:08:39 +0900

    Add files via upload

--- config/security_cfg.py
+++ config/security_cfg.py
@@ -1,10 +1,15 @@
 # config/security_cfg.py
 
+# 허가된 관리자 ID 리스트 (화이트리스트)
+WHITELIST_IDS = ["wschoi", "syno_admin", "seoyeon_choi"]
+
 ADMIN_PASSWORD = "synotech0773!"
-WHITELIST_IDS = ["wschoi", "admin_syno"]
 
-# 보안 모드 상태
-SECURITY_MODE = "Active"
+# 보안 모드 설정
+SECURITY_MODE = "Enforced (ID+PW)"
 
-def check_admin(pw):
-    return pw == ADMIN_PASSWORD
+def verify_admin_access(user_id, password):
+    """ID가 화이트리스트에 있고 비밀번호가 일치하는지 검증"""
+    if user_id in WHITELIST_IDS and password == ADMIN_PASSWORD:
+        return True
+    return False
